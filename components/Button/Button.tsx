81e1daea7f55203342c7b44b7f1ea564cb1f7bac
arnasarn <sirvydasarnas@gmail.com>
2025-08-06 14:12:18 +0300

    work on users heading

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,16 +3,25 @@ import styles from "./button.module.css";
 type ButtonProps = {
   title: string;
   type: string;
+  onClick: () => void;
+  isSelected?: boolean;
   isDisabled?: boolean;
 };
 
-const Button = ({ title, type, isDisabled = false }: ButtonProps) => {
+const Button = ({
+  title,
+  type,
+  onClick,
+  isSelected = false,
+  isDisabled = false,
+}: ButtonProps) => {
   return (
     <button
+      onClick={onClick}
       disabled={isDisabled}
       className={`${styles.main} ${styles[type]} ${
-        isDisabled && styles.disabled
-      }`}
+        isSelected && styles.selected
+      } ${isDisabled && styles.disabled}`}
     >
       {title}
     </button>
--- components/Users/Users.tsx
+++ components/Users/Users.tsx
@@ -1,12 +1,40 @@
+import { useState } from "react";
+import Button from "../Button/Button";
+import SearchBar from "../SearchBar/SearchBar";
 import Cards from "./Cards/Cards";
-import Heading from "./Heading/Heading";
 import styles from "./users.module.css";
 
 const Users = () => {
+  const [isSelectedClients, setSelectedClients] = useState(false);
+  const [isSelectedProviders, setSelectedProviders] = useState(false);
   return (
     <div className={styles.main}>
-      <Heading />
-      <Cards />
+      <div className={styles.heading}>
+        <div className={styles.headingLeftSide}>
+          <Button
+            onClick={() => {
+              setSelectedProviders(false);
+              setSelectedClients(true);
+            }}
+            title="Clients"
+            type="PLAIN"
+            isSelected={isSelectedClients}
+          />
+          <Button
+            onClick={() => {
+              setSelectedProviders(true);
+              setSelectedClients(false);
+            }}
+            title="Providers"
+            type="PLAIN"
+            isSelected={isSelectedProviders}
+          />
+        </div>
+        <div>
+          <SearchBar placeholder="Type user name or ID" />
+        </div>
+      </div>
+      {isSelectedClients && <Cards />}
     </div>
   );
 };
