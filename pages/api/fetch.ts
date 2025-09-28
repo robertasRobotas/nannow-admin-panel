1b649532099e089a0b6d0be9cf66a457f09a921a
Robertas Ankudovicius <r.ankudovicius@gmail.com>
2025-09-28 14:22:52 +0300

    added fake url

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 import Cookies from "js-cookie";
 
-// const BASE_URL = "https://nannow-api.com";
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://nannow-api.com";
+// const BASE_URL = "http://localhost:8080";
 
 const jwt = Cookies.get("@user_jwt");
 
