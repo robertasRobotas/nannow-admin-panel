85912f76b25130fe1c66ee5f5ea89fb735d291e9
Andrii Lazarev <176462708+Lazandrey@users.noreply.github.com>
2026-01-28 16:01:02 +0200

    added email show

--- components/Users/ProfileInfo/ProfileInfo.tsx
+++ components/Users/ProfileInfo/ProfileInfo.tsx
@@ -6,16 +6,29 @@ type ProfileInfoProps = {
   imgUrl: string;
   id: string;
   mode?: "client" | "provider";
+  email: string;
+  locale?: string;
 };
 
-const ProfileInfo = ({ name, imgUrl, id, mode }: ProfileInfoProps) => {
+const ProfileInfo = ({
+  name,
+  imgUrl,
+  id,
+  mode,
+  email,
+  locale,
+}: ProfileInfoProps) => {
   return (
     <div className={styles.profileInfo}>
       <img className={styles.profileImg} src={imgUrl} alt="Profile Image" />
       <span className={`${styles.name} ${nunito.className}`}>
         {name} {mode && (mode === "provider" ? "(Provider)" : "(Client)")}
       </span>
+      <span className={styles.email}>{email}</span>
       <span className={styles.id}>{`USER ID: ${id}`}</span>
+      {locale && (
+        <span className={styles.email}>{`USER LOCALE: ${locale}`}</span>
+      )}
     </div>
   );
 };
