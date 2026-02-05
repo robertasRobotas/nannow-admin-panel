dbb7ab1510c654a454bec58e5dcd970392cd70a6
Robertas Ankudovicius <r.ankudovicius@gmail.com>
2026-02-05 17:09:38 +0200

    Added admin panel review to user link

--- components/Reviews/DetailedReview/DetailedReview.tsx
+++ components/Reviews/DetailedReview/DetailedReview.tsx
@@ -1,14 +1,12 @@
 import styles from "./detailedReview.module.css";
 import { nunito } from "@/helpers/fonts";
 import arrowImg from "../../../assets/images/arrow-right.svg";
-import arrowOut from "../../../assets/images/arrow-out.svg";
 import Button from "@/components/Button/Button";
 import { useMediaQuery } from "react-responsive";
 import { ReviewType } from "@/types/Reviews";
 import { Dispatch, SetStateAction } from "react";
 import avatarImg from "../../../assets/images/default-avatar.png";
-import { toast } from "react-toastify";
-import { copyReview } from "@/helpers/clipboardWrites";
+import Link from "next/link";
 
 type DetailedReview = {
   review: ReviewType;
@@ -17,55 +15,63 @@ type DetailedReview = {
   reviews: ReviewType[];
 };
 
-const DetailedReview = ({
-  review,
-  onBackClick,
-  setReviews,
-  reviews,
-}: DetailedReview) => {
+const DetailedReview = ({ review, onBackClick }: DetailedReview) => {
   const isMobile = useMediaQuery({ query: "(max-width: 936px)" });
 
-  const onShare = async () => {
-    copyReview(review.id);
-    toast("Link to report copied!");
-  };
+  const reviewType = review?.reviewType;
 
-  console.log(review);
+  const reviewerHref =
+    reviewType === "CLIENT_TO_PROVIDER"
+      ? `/client/${review.clientUserId}`
+      : `/provider/${review.providerUserId}`;
+
+  const revieweeHref =
+    reviewType === "CLIENT_TO_PROVIDER"
+      ? `/provider/${review.providerUserId}`
+      : `/client/${review.clientUserId}`;
+
   return (
     <div className={styles.main}>
       <span className={`${styles.sectionTitle} ${nunito.className}`}>
         Details
       </span>
+
       <div className={styles.heading}>
         <div className={styles.reviewDetails}>
-          <div className={styles.profile}>
-            <img src={review?.reviewerImgUrl ?? avatarImg.src} alt="Profile" />
+          <Link href={revieweeHref} className={styles.profile}>
+            <img src={review?.revieweeImgUrl ?? avatarImg.src} alt="Profile" />
             <div>
               <span className={styles.title}>Has been reviewed</span>
               <span className={styles.name}>
-                {`${review?.revieweeFirstName}\n${review?.revieweeSurname}`}
+                {review.revieweeFirstName}
+                <br />
+                {review.revieweeSurname}
               </span>
             </div>
-          </div>
+          </Link>
+
           <img src={arrowImg.src} alt="Arrow" />
-          <div className={styles.profile}>
-            <img src={review?.revieweeImgUrl ?? avatarImg.src} alt="Profile" />
+
+          <Link href={reviewerHref} className={styles.profile}>
+            <img src={review?.reviewerImgUrl ?? avatarImg.src} alt="Profile" />
             <div>
               <span className={styles.title}>Reviewed by</span>
               <span className={styles.name}>
-                {`${review?.reviewerFirstName}\n${review?.reviewerSurname}`}
+                {review.reviewerFirstName}
+                <br />
+                {review.reviewerSurname}
               </span>
             </div>
-          </div>
+          </Link>
         </div>
-        <div className={styles.btnsWrapper}>
-          {isMobile && (
-            <Button title="Back" type="OUTLINED" onClick={onBackClick} />
-          )}
-        </div>
+
+        {isMobile && (
+          <Button title="Back" type="OUTLINED" onClick={onBackClick} />
+        )}
       </div>
+
       <div className={styles.review}>
-        <img src={review?.reviewer?.imgUrl ?? avatarImg.src} alt="Profile" />
+        <img src={review?.reviewerImgUrl ?? avatarImg.src} alt="Profile" />
         <div className={styles.reviewBubble}>
           {review?.text?.trim() ? review.text : "no text was left"}
         </div>
