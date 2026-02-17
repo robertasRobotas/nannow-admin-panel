80f28129fb4d1c2e988788424a5482ec67da2999
Andrii Lazarev <176462708+Lazandrey@users.noreply.github.com>
2026-02-17 18:02:24 +0200

    criminal application history

--- components/CriminalCheck/DetailedCriminalCheck/Document/Document.tsx
+++ components/CriminalCheck/DetailedCriminalCheck/Document/Document.tsx
@@ -1,6 +1,6 @@
 import styles from "./document.module.css";
 import docImg from "../../../../assets/images/doc.svg";
-import downloadImg from "../../../../assets/images/download.svg";
+import arrowOutImg from "../../../../assets/images/arrow-out.svg";
 import Button from "@/components/Button/Button";
 
 type DocumentProps = {
@@ -8,9 +8,15 @@ type DocumentProps = {
 };
 
 const Document = ({ documentUrl }: DocumentProps) => {
-  const handleDownload = () => {
+  const handleOpenDocument = () => {
+    const openedWindow = window.open(documentUrl, "_blank", "noopener,noreferrer");
+    if (openedWindow) return;
+
+    // Popup blockers fallback.
     const a = document.createElement("a");
     a.href = documentUrl;
+    a.target = "_blank";
+    a.rel = "noopener noreferrer";
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
@@ -19,17 +25,17 @@ const Document = ({ documentUrl }: DocumentProps) => {
   return (
     <div className={styles.main}>
       <div className={styles.docInfo}>
-        <img src={docImg.src} />
+        <img src={docImg.src} alt="Document" />
         <span className={styles.title}>UPLOADED DOCUMENT</span>
         <span className={`${styles.fileName} ${styles.nunito}`}>
           {documentUrl.match(/[^\\/]+$/)}
         </span>
       </div>
       <Button
-        title={"Download"}
-        imgUrl={downloadImg.src}
+        title={"Open"}
+        imgUrl={arrowOutImg.src}
         type="WHITE"
-        onClick={() => handleDownload()}
+        onClick={() => handleOpenDocument()}
       />
     </div>
   );
