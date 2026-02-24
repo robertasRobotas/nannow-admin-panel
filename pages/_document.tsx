29e4a618275b7fe2d0b43db254219e0645908a36
Andrii Lazarev <176462708+Lazandrey@users.noreply.github.com>
2026-02-24 21:07:22 +0200

    icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/site-icon.svg" sizes="any" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/site-icon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
