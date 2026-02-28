033350323f62925f22287a67e87f54bb4aa8e41b
Andrii Lazarev <176462708+Lazandrey@users.noreply.github.com>
2026-02-28 11:56:17 +0200

    icon fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,9 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/site-icon.svg" sizes="any" type="image/svg+xml" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/site-icon.svg" />
+        <link
+          rel="icon"
+          href="/site-icon.svg?v=2"
+          sizes="any"
+          type="image/svg+xml"
+        />
+        <link rel="shortcut icon" href="/site-icon.svg?v=2" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/site-icon.svg?v=2" />
       </Head>
       <body>
         <Main />
