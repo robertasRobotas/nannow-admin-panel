e3f524e70bd8f17e15280b17002736f44148202f
Andrii Lazarev <176462708+Lazandrey@users.noreply.github.com>
2026-02-28 11:51:37 +0200

    site icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,14 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { inter } from "@/helpers/fonts";
 import { ToastContainer } from "react-toastify";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
+      <Head>
+        <title>Nannow admin</title>
+      </Head>
       <Component {...pageProps} />
       <ToastContainer position="bottom-right" />
     </main>
